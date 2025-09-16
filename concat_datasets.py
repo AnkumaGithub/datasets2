d29dea4936ff0b8e0017fa3c2bcafefd7526d26f
Maksim <143986439+AnkumaGithub@users.noreply.github.com>
2025-09-17 01:57:22 +0300

    hehe2

--- concat_datasets.py
+++ concat_datasets.py
@@ -22,19 +22,23 @@ df4['label'] = df4['type'].map({'benign': 0, 'jailbreak': 1})
 df4 = df4.rename(columns={'prompt': 'text'})[['text', 'label']]
 
 # 5. Локальный файл 2
-df5 = pd.read_csv('/Users/maksimkuznetsov/Downloads/synthetic_dataset.csv')
+df5 = pd.read_csv('/Users/maksimkuznetsov/Downloads/raw_dataset.csv')
+print(df5['jailbreak'][:5])
+# Приведите строки к верхнему регистру и удалите пробелы
+df5['jailbreak'] = df5['jailbreak'].str.strip().str.upper()
+df5['jailbreak'] = df5['jailbreak'].apply(lambda x: 1 if x == 'TRUE' else 0)
+
 df5 = df5.rename(columns={
     'prompt': 'text',
     'jailbreak': 'label'
 })[['text', 'label']]
 
-# Объединение всех датасетов
 combined_df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
 
 # Удаление дубликатов и пустых значений
 combined_df = combined_df.dropna().drop_duplicates()
 
 # Сохранение результата
-combined_df.to_csv('combined_jailbreak_dataset.csv', index=False)
+combined_df.to_csv('combined_jailbreak_dataset2.csv', index=False)
 
 print(f"Объединенный датасет сохранен. Размер: {combined_df.shape}")
